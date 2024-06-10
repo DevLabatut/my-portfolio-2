c21887a1e35e100e7ea0c5924232b10e74d19a24
Lucas Labatut <lucas.labatut.sw@gmail.com>
2024-06-10 05:02:27 -0300

    Added JPG images, created 'vid' folder for videos, implemented 'mobile.css' for responsiveness, removed all comments, made some alterations and additions to the JavaScript code

--- js/popupManager.js
+++ js/popupManager.js
@@ -2,35 +2,53 @@ document.addEventListener("DOMContentLoaded", function () {
   const abrirBtn = document.getElementById("abrir");
   const overlay = document.getElementById("overlay");
   const popup1 = document.getElementById("popup-1");
-  const popup2 = document.getElementById("popup-2");
+  const popups = document.querySelectorAll(".popup-2");
   const itensMenu = document.querySelectorAll("#popup-1 li a");
   const buttons = document.querySelectorAll(".dd-demo");
+  const closeIcons = document.querySelectorAll(".close-over");
 
-  // Função para exibir/ocultar overlay e popup com efeito de transição suave
-  function togglePopupDisplay(overlay, popup, action) {
+  const videoMap = {
+    "Portfólio - Lucas Labatut": "assets/vid/video-portfolio-labatut.mp4",
+    "Landing Page Dark/Light": "assets/vid/landing-page-dark-light.mp4",
+    "Lista de Tarefas": "assets/vid/lista-de-tarefas.mp4",
+    Calculadora: "assets/vid/calculadora.mp4",
+    "Clone Facebook": "assets/vid/clone-facebook.mp4",
+    "Validação de Login": "assets/vid/validação-de-login.mp4",
+  };
+
+  function showVideo(projectTitle, popup) {
+    const videoSource = videoMap[projectTitle];
+    if (videoSource) {
+      popup.querySelector("video").src = videoSource;
+    }
+  }
+
+  function togglePopupDisplay(overlay, popup, action, smoothTransition = true) {
     if (action === "block") {
       overlay.style.opacity = "0";
       overlay.style.display = "block";
       popup.style.opacity = "0";
       popup.style.display = "block";
-
-      // Gradualmente aumentar a opacidade do overlay e do popup
-      setTimeout(function () {
+      if (smoothTransition) {
+        setTimeout(function () {
+          overlay.style.opacity = "1";
+          popup.style.opacity = "1";
+        }, 0);
+      } else {
         overlay.style.opacity = "1";
         popup.style.opacity = "1";
-      }, 100);
+      }
     } else {
       overlay.style.opacity = "0";
       popup.style.opacity = "0";
-
-      // Esperar a transição terminar antes de ocultar o overlay e o popup
-      setTimeout(function () {
-        overlay.style.display = "none";
-        popup.style.display = "none";
-      }, 500); // Tempo correspondente à duração da transição CSS
+      setTimeout(
+        function () {
+          overlay.style.display = "none";
+          popup.style.display = "none";
+        },
+        smoothTransition ? 500 : 0
+      );
     }
-
-    // Ativar ou desativar rolagem do corpo
     if (action === "block") {
       document.body.classList.add("no-scroll");
     } else {
@@ -43,19 +61,36 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   overlay.addEventListener("click", function () {
-    togglePopupDisplay(overlay, popup1, "none");
-    togglePopupDisplay(overlay, popup2, "none");
+    closeAllPopups();
   });
 
   itensMenu.forEach(function (item) {
     item.addEventListener("click", function () {
-      togglePopupDisplay(overlay, popup1, "none");
+      togglePopupDisplay(overlay, popup1, "none", false);
     });
   });
 
-  buttons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      togglePopupDisplay(overlay, popup2, "block");
+  buttons.forEach(function (button, index) {
+    button.addEventListener("click", function (event) {
+      const projectTitle = button.previousElementSibling.textContent;
+      const popup = popups[index];
+      const popupTitle = popup.querySelector(".dal-titul");
+      popupTitle.textContent = projectTitle;
+      showVideo(projectTitle, popup);
+      togglePopupDisplay(overlay, popup, "block");
     });
   });
+
+  closeIcons.forEach(function (closeIcon) {
+    closeIcon.addEventListener("click", function () {
+      closeAllPopups();
+    });
+  });
+
+  function closeAllPopups() {
+    togglePopupDisplay(overlay, popup1, "none", false);
+    popups.forEach((popup) =>
+      togglePopupDisplay(overlay, popup, "none", false)
+    );
+  }
 });
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,62 +1,55 @@
-// Verificar se é um dispositivo móvel
 function isMobileDevice() {
-    return (
-      typeof window.orientation !== "undefined" ||
-      navigator.userAgent.indexOf("IEMobile") !== -1
-    );
-  }
-  
-  // Função para rolagem suave (somente para desktop)
-  function smoothScroll(target, duration, itensMenu) { // Adicionando 'itensMenu' como um parâmetro
-    if (!isMobileDevice()) {
-      var targetElement = document.querySelector(target);
-      var targetPosition =
-        targetElement.getBoundingClientRect().top + window.pageYOffset - 30; // Ajuste para o padding da seção
-      var startPosition = window.pageYOffset;
-      var distance = targetPosition - startPosition;
-      var startTime = null;
-  
-      function animation(currentTime) {
-        if (startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
-        var ease = easeInOutQuad(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0, ease);
-        if (timeElapsed < duration) requestAnimationFrame(animation);
-      }
-  
-      function easeInOutQuad(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return (c / 2) * t * t + b;
-        t--;
-        return (-c / 2) * (t * (t - 2) - 1) + b;
-      }
-  
-      requestAnimationFrame(animation);
-    } else {
-      // Se for um dispositivo móvel, simplesmente navegue para o destino sem rolagem suave
-      var targetElement = document.querySelector(target);
-      targetElement.scrollIntoView({ behavior: "auto", block: "start" });
+  return (
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
+  );
+}
+
+function smoothScroll(target, duration, itensMenu) {
+  if (!isMobileDevice()) {
+    var targetElement = document.querySelector(target);
+    var targetPosition =
+      targetElement.getBoundingClientRect().top + window.pageYOffset - 30;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      var timeElapsed = currentTime - startTime;
+      var ease = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, ease);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
     }
-  }
-  
-  // Adicionar ouvintes de eventos para links dentro do popup (somente para desktop)
-  document.addEventListener("DOMContentLoaded", function () {
-    if (!isMobileDevice()) {
-      var itensMenu = document.querySelectorAll("#popup-1 li a"); // Definindo 'itensMenu' aqui
-      itensMenu.forEach(function (item) {
-        item.addEventListener("click", function (event) {
-          event.preventDefault(); // Impedir o comportamento padrão do link
-          var target = this.getAttribute("href");
-          smoothScroll(target, 700, itensMenu); // Passando 'itensMenu' como um parâmetro
-        });
-      });
-  
-      // Adicionar ouvinte de eventos para o botão de contato
-      var botaoContato = document.querySelector(".botao-contato");
-      botaoContato.addEventListener("click", function (event) {
-        event.preventDefault(); // Impedir o comportamento padrão do link
-        smoothScroll("#contato", 700, itensMenu); // Passando 'itensMenu' como um parâmetro
-      });
+
+    function easeInOutQuad(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
     }
-  });
-  
+
+    requestAnimationFrame(animation);
+  } else {
+    var targetElement = document.querySelector(target);
+    targetElement.scrollIntoView({ behavior: "auto", block: "start" });
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  if (!isMobileDevice()) {
+    var itensMenu = document.querySelectorAll("#popup-1 li a");
+    itensMenu.forEach(function (item) {
+      item.addEventListener("click", function (event) {
+        event.preventDefault();
+        var target = this.getAttribute("href");
+        smoothScroll(target, 700, itensMenu);
+      });
+    });
+
+    var botaoContato = document.querySelector(".botao-contato");
+    botaoContato.addEventListener("click", function (event) {
+      event.preventDefault();
+      smoothScroll("#contato", 700, itensMenu);
+    });
+  }
+});
